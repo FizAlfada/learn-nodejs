81f8178641d6597478b1fd1e242185b5326c9474
fizalfada <justinsp1re212@gmail.com>
2024-05-30 14:14:42 +0700

    menambahkan module URL

---
+++ 08-URL-Module/index.js
@@ -0,0 +1,23 @@
+import url from "url";
+
+// const myUrl = new URL("https://example.org:8080/foo#bar");
+// const myUrl = new URL("http://example.org:8080/foo/bar?test");
+const myUrl = new URL("http://example.org:8080/foo/v?watch=test");
+
+// console.log(myUrl.hash);
+// console.log(myUrl.host);
+// console.log(myUrl.hostname);
+// console.log(myUrl.href);
+// console.log(myUrl.origin);
+// console.log(myUrl.pathname);
+// console.log(myUrl.port);
+// console.log(myUrl.protocol);
+// console.log(myUrl.search);
+// console.log(myUrl.searchParams);
+// console.log(myUrl.username);
+
+// both are the same
+// console.log(myUrl.toString());
+// console.log(myUrl.toString());
+
+// console.log(myUrl.searchParams.get("watch"));
