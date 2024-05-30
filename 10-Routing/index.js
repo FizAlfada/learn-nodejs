2fe0bde70c810827ed9c8df9b533226035ecb8a6
fizalfada <justinsp1re212@gmail.com>
2024-05-30 22:20:48 +0700

    mengubah server.listen

--- 10-Routing/index.js
+++ 10-Routing/index.js
@@ -18,4 +18,10 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(port, () => console.log("Server ready!"));
+server.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Server running on port ${port}`);
+  }
+});
